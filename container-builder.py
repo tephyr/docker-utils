73c7d89339a89d968e0861f98a3ff00dc45900d4
Andrew Ittner <aji@rhymingpanda.com>
2019-04-30 12:17:07 -0700

    Show more container info, all available keys

--- container-builder.py
+++ container-builder.py
@@ -1,15 +1,27 @@
 import docker
 
-def main():
+def show_current_containers():
 
     client = docker.from_env(version='auto')
+    
+    last_name = None
     
     for container in client.containers.list():
         #print(container)
         #print(container.attrs)
         name = container.attrs['Name']
-        image = container.attrs['Config']['Image']
-        print(f"{container}: {name!s} from {image!s}")
+        last_name = name
+        imagePrimary = container.attrs['Image']
+        imageAttr = container.attrs['Config']['Image']
+        print(f"{container}: {name}, {imageAttr}, {imagePrimary}")
 
+    if last_name is not None:
+        container = client.containers.get(last_name)
+        print(container.attrs.keys())
+    
+def main():
+
+    show_current_containers()
+    
 if __name__ == '__main__':
     main()
